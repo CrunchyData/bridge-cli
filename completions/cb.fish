cafee01a2198fc3885b006ab3da5b1587588f17b
Will Leinweber <will@bitfission.com>
2021-07-03 18:23:16 -0700

    Remove git version stuff, add completions dir and fish completion

---
+++ completions/cb.fish
@@ -0,0 +1,1 @@
+complete --command cb --arguments '(cb --_completion (commandline -cp))' --no-files
--- src/cb.cr
+++ src/cb.cr
@@ -2,17 +2,11 @@ module CB
   EID_PATTERN = /\A[a-z0-9]{25}[4aeimquy]\z/
 
   VERSION       = {{ `shards version "#{__DIR__}"`.chomp.stringify }}
-  BUILD_COMMIT  = {{ `git rev-parse --short HEAD`.strip.stringify }}
   BUILD_RELEASE = {{ flag?(:release) }}
   BUILD_DATE    = {{ `date -u +"%Y%m%d%H%M"`.chomp.stringify }}
-  BUILD_CLEAN   = begin
-    {% begin %}
-      {{`git diff-index --quiet HEAD -- && echo true || echo false`}}
-    {% end %}
-  end
   BUILD_ID = begin
     {% begin %}
-      %(#{"+" unless BUILD_CLEAN}#{BUILD_COMMIT}-#{BUILD_DATE}#{"-dev" unless BUILD_RELEASE})
+      %(#{BUILD_DATE}#{"-dev" unless BUILD_RELEASE})
     {% end %}
   end
 end
