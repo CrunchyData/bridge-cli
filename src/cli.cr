0eff477b75ded369b303593a4b6947451ab78e35
Adam Brightwell <adam.brightwell@crunchydata.com>
2022-03-28 13:07:34 -0400

    Add custom option parser `to_s`.
    This adds an override of `OptionParser#to_s`. This override provides a
    custom presentation of how subcommands and flags are presented when the
    `--help` option is provided.  Specifically, it explicitly separates
    `commands` from `flags`. As well, it sorts both alphabetically before
    presenting them.

--- src/cli.cr
+++ src/cli.cr
@@ -34,6 +34,37 @@ class OptionParser
     flag, value_type = parse_flag_definition(flag)
     @handlers[flag] = Handler.new(value_type, block)
   end
+
+  def to_s(io : IO) : Nil
+    if banner = @banner
+      io << banner
+      io << '\n'
+    end
+
+    commands = [] of String
+    flags = [] of String
+
+    # filter out commands and flags
+    @flags.each do |flag|
+      if flag.lstrip.starts_with? "-"
+        flags << flag
+      else
+        commands << flag
+      end
+    end
+
+    if !commands.empty?
+      io << "\n" << "Available Commands".colorize.bold <<":\n"
+      io << commands.sort.join io, "\n"
+      io << "\n"
+    end
+
+    if !flags.empty?
+      io << "\n" << "Options".colorize.bold << ":\n"
+      flags.sort! { |a, b| a.lstrip(" -") <=> b.lstrip(" -") }
+      flags.join io, '\n'
+    end
+  end
 end
 
 action = nil
