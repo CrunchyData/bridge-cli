ab7b69a3802a6575338ac0842cabd579fdb747d9
Adam Brightwell <adam.brightwell@crunchydata.com>
2022-09-12 13:50:55 -0400

    Add API_NAME_PATTERN.
    The API has some restrictions on what is allowed for user provided names
    of resources, for instance team or cluster names.  So, here we are
    adding a pattern for validating such names.

--- src/cb.cr
+++ src/cb.cr
@@ -1,7 +1,16 @@
 module CB
   # Global application constants.
+
   EID_PATTERN = /\A[a-z0-9]{25}[4aeimquy]\z/
-  HOST        = ENV["CB_HOST"]? || "api.crunchybridge.com"
+
+  # For simple identifiers such as region names, or plan names where we
+  # expect only lowercase, numbers, and -
+  IDENT_PATTERN = /\A[a-z0-9\-]+\z/
+
+  # For user specified API resource names such as cluster and team names.
+  API_NAME_PATTERN = /\A[\p{L}\p{So}][\p{L}\p{N}\p{So}\/\-_ ']{3,48}[\p{L}\p{N}\p{So}]\z/
+
+  HOST = ENV["CB_HOST"]? || "api.crunchybridge.com"
 
   # Release constants.
   BUILD_RELEASE  = {{ flag?(:release) }}
