38dfb34c349a2b11e4b8aa8dfdb7289c70e1203e
Adam Brightwell <adam.brightwell@crunchydata.com>
2022-10-24 16:16:45 -0400

    Fix cluster id parsing with `cb logs`

--- src/cb/tempkey.cr
+++ src/cb/tempkey.cr
@@ -2,7 +2,7 @@ module CB
   record Tempkey, private_key : String, public_key : String, cluster_id : String, team_id : String, expires_at : Time do
     Cacheable.include key: cluster_id
 
-    def self.for_cluster(cluster_id, client) : Tempkey
+    def self.for_cluster(cluster_id : Identifier, client) : Tempkey
       tk = fetch? cluster_id
       return tk if tk
       client.get_tempkey(cluster_id).store
