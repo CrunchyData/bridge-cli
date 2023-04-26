e9733ed95297b58e05dfcf189ef993bd7823ac15
Adam Brightwell <adam.brightwell@crunchydata.com>
2023-04-26 12:51:19 -0400

    Handle missing `host` and `network` for `fork` and `replica`.
    When creating a fork or a replica it is possible that the host and
    network are not yet available. So, here we're accounting for that by
    displaying both as `Not available`.

--- src/cb/cluster_info.cr
+++ src/cb/cluster_info.cr
@@ -19,7 +19,7 @@ class CB::ClusterInfo < CB::APIAction
 
     details = {
       "state"              => cluster_status.state,
-      "host"               => c.host,
+      "host"               => c.host.empty? ? "Not available" : c.host,
       "created"            => c.created_at.to_rfc3339,
       "plan"               => "#{c.plan_id} (#{format(c.memory)}GiB ram, #{format(c.cpu)}vCPU)",
       "version"            => c.major_version,
@@ -34,7 +34,7 @@ class CB::ClusterInfo < CB::APIAction
       details["source cluster"] = source
     end
 
-    details["network"] = c.network_id if c.network_id
+    details["network"] = c.network_id ? c.network_id.to_s : "Not available"
 
     pad = (details.keys.map(&.size).max || 8) + 2
     details.each do |k, v|
@@ -42,13 +42,15 @@ class CB::ClusterInfo < CB::APIAction
       output << v << "\n"
     end
 
-    firewall_rules = client.get_firewall_rules c.network_id
-    output << "firewall".rjust(pad).colorize.bold << ": "
-    if firewall_rules.empty?
-      output << "no rules\n"
-    else
-      output << "allowed cidrs".colorize.underline << "\n"
+    if c.network_id
+      firewall_rules = client.get_firewall_rules c.network_id
+      output << "firewall".rjust(pad).colorize.bold << ": "
+      if firewall_rules.empty?
+        output << "no rules\n"
+      else
+        output << "allowed cidrs".colorize.underline << "\n"
+      end
+      firewall_rules.each { |fr| output << " "*(pad + 4) << fr.rule << "\n" }
     end
-    firewall_rules.each { |fr| output << " "*(pad + 4) << fr.rule << "\n" }
   end
 end
