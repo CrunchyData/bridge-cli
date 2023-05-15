6859d026f17effffbc43aae18a2b4b50ddc12d12
Adam Brightwell <adam.brightwell@crunchydata.com>
2023-05-15 16:42:36 -0400

    Add api key validation checks and error message to `cb login`.

--- src/cb/login.cr
+++ src/cb/login.cr
@@ -10,12 +10,28 @@ class CB::Login < CB::Action
 
     output.print "  application secret: "
     secret = input.noecho { input.gets }
-    output.print "\n"
+
     if secret.nil? || secret.empty?
       STDERR.puts "#{"error".colorize.red.bold}: application secret must be present"
       exit 1
     end
+    output.print "\n"
 
-    Creds.new(host, secret).store
+    secret = secret.strip
+
+    unless secret.starts_with? "cbkey_"
+      msg = "\n#{"error".colorize.red.bold}: The key provided is not a valid API key.\n" \
+            "  Verify that you are using an API key that is prefixed with 'cbkey_'.\n" \
+            "  You can manage your API keys here: https://crunchybridge.com/account/api-keys\n"
+
+      STDERR.puts msg
+      exit 1
+    end
+
+    output << "Storing credentials... "
+    creds = Creds.new(host, secret).store
+    status = creds ? "OK".colorize.green.bold : "Failed".colorize.red.bold
+    output << "#{status}\n"
+    creds
   end
 end
