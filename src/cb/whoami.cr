35fddb95844892ed1c4856d48a90b4a17c283de4
Adam Brightwell <adam.brightwell@crunchydata.com>
2023-05-24 14:51:53 -0400

    Update `whoami` to fetch user information from API.
    Previously, `whoami` was relying fetching information from a token. The
    problem with this is that this information might not always be
    up-to-date or accurate. As a token is cached locally and the a different
    user might have 'logged in'. And since the 'login' process does not
    update the current token (this happens once the token expires,
    admittedly a poor design, but improvements are incoming), a user could
    'login' as a different user but still be given the previous user info
    from `whoami`. This is certainly a 'bug' and this is the just the first
    step in fixing this issue.

--- src/cb/whoami.cr
+++ src/cb/whoami.cr
@@ -2,8 +2,10 @@ require "./action"
 
 class CB::WhoAmI < CB::APIAction
   def run
-    output << "user id: ".colorize.t_id << client.token.user_id << "\n"
-    output << "   name: ".colorize.t_id << client.token.name << "\n"
-    output << "   host: ".colorize.t_id << client.host << "\n"
+    account = client.get_account
+
+    output << "user id: ".colorize.t_id << account.id << "\n"
+    output << "   name: ".colorize.t_id << account.name << "\n"
+    output << "   host: ".colorize.t_id << CB::HOST << "\n"
   end
 end
